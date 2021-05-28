5959c2585c5b918ad82630e2ff4822e3fe0438de
xxbbgun <Kanticha_sr@cmu.ac.th>
2021-05-28 16:53:43 +0700

    alert button

--- myScript.js
+++ myScript.js
@@ -1,3 +1,15 @@
 document.getElementById('cancel').addEventListener('mousemove', (event) => {
 	console.log(`Mouse X: ${event.clientX}, Mouse Y: ${event.clientY}`);
 });
+document.getElementById('ok').addEventListener('click',function(e){
+    alert('ok')
+}
+)
+document.getElementById('cancel').addEventListener('click',function(e){
+    alert('cancel')
+}
+)
+document.getElementById('no').addEventListener('click',function(e){
+    alert('no')
+}
+)
