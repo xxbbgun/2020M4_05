d785c7b2eea059301d5fd84f7884f6137bfbcc20
xxbbgun <Kanticha_sr@cmu.ac.th>
2021-05-28 16:52:53 +0700

    mousemove

--- myScript.js
+++ myScript.js
@@ -1,4 +1,3 @@
-document.getElementById('ok').addEventListener('click',function(e){
-    alert('ok is clicked')
-}
-)
+document.getElementById('cancel').addEventListener('mousemove', (event) => {
+	console.log(`Mouse X: ${event.clientX}, Mouse Y: ${event.clientY}`);
+});
